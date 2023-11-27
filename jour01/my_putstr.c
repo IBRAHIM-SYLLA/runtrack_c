5b95bebbbe58ae2307ffa1a300b45c3e9a2df2f1
ibrahim-sylla <91750360+IBRAHIM-SYLLA@users.noreply.github.com>
2023-11-27 13:14:29 +0100

    job01 done

---
+++ jour01/my_putstr.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+void my_putchar(char c) {
+    write(1, &c, 1);
+}
+
+void my_putstr(char *str){
+    for(int i = 0; str[i] != 0; i++){
+        my_putchar(str[i]);
+    }
+}
